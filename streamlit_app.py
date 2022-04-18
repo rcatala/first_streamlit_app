23c5eabbfeb55629ee20b6bf6b21eeb238d9132b
Rama <catala_ramiro@hotmail.com>
2022-04-18 12:16:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,11 @@ streamlit.text('🍌🍓 Build your Own Fruit Smoothie 🍇🥝')
 
 import pandas
 
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
+streamlit.multiselect("Pick some fuits:", list (my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
